cba62550e673d41e100376653dcba973fdd9c284
Mojisolar <mojisolatitilope92@gmail.com>
2023-03-17 11:12:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that print a line to standard error
- * Return: 1 (success)
+ * Return: 1
  */
-int main (void)
-{  
+int main(void)
+{
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
