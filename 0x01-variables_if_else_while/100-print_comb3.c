1af679e3f6bf7bb9d70298065b9b5d7cb8a70e96
Mojisolar <mojisolatitilope92@gmail.com>
2023-03-19 23:38:29 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  prints all possible different combinations of two digits.
+ * Return: Always (success)
+ *
+ */
+int main(void)
+{
+	int d, e, f;
+
+	d = 0;
+	while (d < 100)
+
+	{
+		e = d % 10; /* singles digit */
+		f = d / 10; /* doubles digit */
+
+		if (f < e)
+		{
+			putchar(e + '0');
+			putchar(f + '0');
+
+
+			if (d < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		d++
+	}
+	putchar('\n');
+	return (0);
+}
