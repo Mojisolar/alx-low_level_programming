f68f3111c727bb4d5d166fa4cbc37b2131d5e6f6
Mojisolar <mojisolatitilope92@gmail.com>
2023-03-19 17:02:00 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,17 +14,19 @@ int main(void)
 		b = a / 100; /* hundreds */
 		c = (a / 10) % 10; /* tens */
 		d = a % 10; /* singles */
-	}
-	if (b < c && c < d)
-	{
-		putchar(b + '0');
-		putchar(c + '0');
-		putchar(d + '0');
-	}
-	if  (a < 700)
-	{
-		putchar(44);
-		putchar(32);
+		
+		if (b < c && c < d)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+			
+			if  (a < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
