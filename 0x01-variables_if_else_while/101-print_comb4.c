99b9d3a654cb8cd90b967a82011e8409199ce198
Mojisolar <mojisolatitilope92@gmail.com>
2023-03-19 17:06:09 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,13 +14,11 @@ int main(void)
 		b = a / 100; /* hundreds */
 		c = (a / 10) % 10; /* tens */
 		d = a % 10; /* singles */
-		
 		if (b < c && c < d)
 		{
 			putchar(b + '0');
 			putchar(c + '0');
 			putchar(d + '0');
-			
 			if  (a < 700)
 			{
 				putchar(44);
