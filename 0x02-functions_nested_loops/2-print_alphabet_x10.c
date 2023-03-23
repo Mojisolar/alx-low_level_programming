74f7240db2b03854263946bc43b65bcfe40174dd
Mojisolar <mojisolatitilope92@gmail.com>
2023-03-22 19:24:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make 10 times the alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char p;
+	int b = 0;
+	while (b <= 0)
+	
+		for (p = 'a'; p <= 'z'; p++)
+	
+		{
+			_putchar(p);
+			_putchar('\n');
+			b++;
+		}
+}
